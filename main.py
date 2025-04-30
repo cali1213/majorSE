938a723d9b71a2ee25ff2d0b08b541cfdc847361
cali1213 <124484408+cali1213@users.noreply.github.com>
2025-04-30 12:33:44 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
+import random 
+def sunflower_health(rain_level, nutrient_heath,winds, atmposphere, uv_level, cloud_level, soil_quality, ph_balance, organic_content, temperatre, fertilizer):
+  soil_quality = 80 
+  nutrient_health = 70 
+  rain_level = 5 
+  winds = 3
+
+  def temp(temperature):
+    min_value = 9
+    max_value = 37 
+    temperature = np.arrange(min_value, max_value + 1) 
+          
+  while uv_level => 10 AND uv_level =< 0:
+     return True 
+     if uv_level == 8:
+       soil_quality = 40
+       nutrient_health = 60
+       rain_level = - 10 
+     elif uv_level =< 10 AND uv_level <= 0:
+         print("uv_level invalid. Reset game.")
+         return False
+
+
+
+
+
+       
+       
+       
+      
+    
+
+
+  
+    
+  rain_level = 0
+  nutrient_health = 90
+  uv_level = 8
+  soil_quality = 60/100
 
