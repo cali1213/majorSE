c23735a2ce3f1b1a31b63651b66a26c3bdc67296
cali1213 <124484408+cali1213@users.noreply.github.com>
2025-05-02 11:57:21 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import numpy as np 
 import random 
 def sunflower_health(rain_level, nutrient_heath,winds, atmposphere, uv_level, cloud_level, soil_quality, ph_balance, organic_content, temperatre, fertilizer):
   soil_quality = 80 
   nutrient_health = 70 
   rain_level = 5 
-  winds = 3
+  winds = 3 
+  rain_level = 0
+  nutrient_health = 90
+  uv_level = 8
+  soil_quality = 60/100
 
   def temp(temperature):
     min_value = 9
     max_value = 37 
     temperature = np.arrange(min_value, max_value + 1) 
-          
+
+
   while uv_level => 10 AND uv_level =< 0:
      return True 
      if uv_level == 8:
@@ -19,6 +25,27 @@ def sunflower_health(rain_level, nutrient_heath,winds, atmposphere, uv_level, cl
      elif uv_level =< 10 AND uv_level <= 0:
          print("uv_level invalid. Reset game.")
          return False
+
+def generate_daily_temps(month, avg, tempdata):
+  temp_data = {
+  'december': [11, 22, 23, 12,23,24, 26, 24, 22], 
+  'January':  [13, 23, 23, 14, 26, 24, 24, 16, 17], 
+  'february': [21, 13, 16, 23, 25, 26, 23, 17, 18], 
+  {
+
+    month_length = 30 
+    daily_temps = np.randon.normal(loc=avg_tempdata, scale = 2.0, size=days_in_month)
+                                  
+                                   
+  def calculate_avg_temp(data):
+    avg_temp = {}
+    
+
+  december_tempdata = [11, 22, 23, 12,23,24]
+  
+  daily_ average = average_temperature(december_tempdata)
+
+
 
 
 
@@ -32,9 +59,5 @@ def sunflower_health(rain_level, nutrient_heath,winds, atmposphere, uv_level, cl
 
 
   
-    
-  rain_level = 0
-  nutrient_health = 90
-  uv_level = 8
-  soil_quality = 60/100
+  
 
