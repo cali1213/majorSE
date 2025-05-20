dec2559e92b1f4423ef1a989d5de80f550ebab29
cali1213 <124484408+cali1213@users.noreply.github.com>
2025-05-20 13:06:58 +1000

    Create sunflower.py

---
+++ sunflower.py
@@ -0,0 +1,18 @@
+import numpy as np 
+import randon 
+
+class sunflower_health: 
+  def__init__(self, name): 
+  self.name = name
+  self.sunlight = 50
+  
+
+
+def water_planet
+
+def check_health(self): 
+  average_health = (self.water_level + self.sunlight + self.n
+  if average_health > 75: 
+                    status = "Healthy:
+
+class rain_levels: 
